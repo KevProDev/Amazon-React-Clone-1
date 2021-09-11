4e20b7f5998eaa53167334569bd474bbe929a52f
KevProDev <kevintheleader@yahoo.com>
2021-09-11 18:25:42 -0500

    add & remove checkout

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGAqEoET-ztvFe0eGZnv3HrGM1FA24oUU",
+  authDomain: "amzn-e8ef9.firebaseapp.com",
+  projectId: "amzn-e8ef9",
+  storageBucket: "amzn-e8ef9.appspot.com",
+  messagingSenderId: "352914442866",
+  appId: "1:352914442866:web:21e228987c3d8afc99d0eb",
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,48 +8,57 @@ import {
   ShoppingIcon,
 } from "@heroicons/react/outline";
 import LiveSearch from "./LiveSearch";
+import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
-function Header({ products }) {
-  console.log("App Render Starts");
-  const [search, setSearch] = useState("");
-  const [records, setRecord] = useState([]);
-  const [filterProducts, setFilterProducts] = useState([]);
+function Header() {
+  const [session] = useSession();
+  const router = useRouter();
+  const items = useSelector(selectItems);
 
-  const water = search;
+  // console.log("App Render Starts");
+  // const [search, setSearch] = useState("");
+  // const [records, setRecord] = useState([]);
+  // const [filterProducts, setFilterProducts] = useState([]);
 
-  const loadProductDetail = () => {
-    setRecord(products);
-    // setRecord((products) => [...products, products]);
-    console.log("setting products into SetRecord");
-    console.log(products);
-  };
+  // const water = search;
 
-  useEffect(() => {
-    console.log("use Effect started to set Product into Records");
-    loadProductDetail();
-  }, []);
+  // const loadProductDetail = () => {
+  //   setRecord(products);
+  //   // setRecord((products) => [...products, products]);
+  //   console.log("setting products into SetRecord");
+  //   console.log(products);
+  // };
 
-  useEffect(() => {
-    if (search.length > 0) {
-      console.log("useEffect for search is");
-      console.log(search);
-      setFilterProducts([]);
-      setFilterProducts(
-        products.filter((product) => {
-          return product.title.toLowerCase().includes(search.toLowerCase());
-        })
-      );
-    } else {
-      setFilterProducts([]);
-    }
-  }, [search]);
+  // useEffect(() => {
+  //   console.log("use Effect started to set Product into Records");
+  //   loadProductDetail();
+  // }, []);
+
+  // useEffect(() => {
+  //   if (search.length > 0) {
+  //     console.log("useEffect for search is");
+  //     console.log(search);
+  //     setFilterProducts([]);
+  //     setFilterProducts(
+  //       products.filter((product) => {
+  //         return product.title.toLowerCase().includes(search.toLowerCase());
+  //       })
+  //     );
+  //   } else {
+  //     setFilterProducts([]);
+  //   }
+  // }, [search]);
 
   return (
     <header>
-      {console.log("HTML started")}
+      {/* {console.log("HTML started")} */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex item-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -63,26 +72,28 @@ function Header({ products }) {
             className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none"
             type="text"
             id="search"
-            placeholder="Enter What You're Looking For"
-            onChange={(e) => setSearch(e.target.value)}
-            value={search}
+            // onChange={(e) => setSearch(e.target.value)}
+            // value={search}
           />
           <SearchIcon className="h-12 p-4" />
         </div>
 
         {/* RIGHT  */}
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div className="link">
-            <p>Hello Kevin Johnson</p>
+          <div onClick={!session ? signIn : signOut} className="link">
+            <p>{session ? `Hello, ${session.user.name}` : "Sign In"}</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
           <div className="link">
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="relative link flex items-center">
+          <div
+            onClick={() => router.push("/checkout")}
+            className="relative link flex items-center"
+          >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black text-bold">
-              0
+              {items.length}
             </span>
             <ShoppingCartIcon className="h-10" />
             <p className="hidden md:inline mt-2 font-extrabold md:text-sm">
@@ -91,18 +102,14 @@ function Header({ products }) {
           </div>
         </div>
       </div>
-      <div>{search}</div>
+      {/* <div>{search}</div>
       <div>
-        {/* {products.map((product) => (
-          <p key={product.id}>{product.title}</p>
-        ))} */}
-
         {filterProducts.map((product, index) => (
           <p className="text-xs text-gray-500" key={index}>
             {product.title}
           </p>
         ))}
-      </div>
+      </div> */}
       <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
         <p className="link flex items-center">
           <MenuIcon className="h-6 mr-1" />
