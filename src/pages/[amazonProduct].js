0dcc00f44346c0f47116f310fb1fffaa81ba0b6e
KevProDev <kevintheleader@yahoo.com>
2021-10-18 18:14:28 -0500

    finish styling the product page

--- src/pages/[amazonProduct].js
+++ src/pages/[amazonProduct].js
@@ -2,40 +2,49 @@ import React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Image from "next/image";
+import Header from "../components/Header";
 
 export default function amazonProduct({ amazonProduct }) {
   // const router = useRouter();
   // const { id } = router.query;
   return (
-    <div className="bg-blue-300 grid px-8 py-12 sm:grid-rows-3 md:grid-cols-5 max-w-screen-xl m-auto">
-      {/* Image Left */}
-      <div className="col-span-2">
-        <Image
-          src={amazonProduct.image}
-          height={200}
-          width={200}
-          objectFit="contain"
-        />
-      </div>
+    <div>
+      <Header />
+      <div className="grid px-8 py-12 sm:grid-rows-3 mt-4 md:grid-cols-5 max-w-screen-xl m-auto space-y-3">
+        {/* Image Left */}
+        <div className="col-span-2 flex items-center justify-center sm:justify-end">
+          <Image
+            src={amazonProduct.image}
+            height={300}
+            width={300}
+            objectFit="contain"
+          />
+        </div>
 
-      {/* Info Middle */}
-      <div className="col-span-2 space-y-3">
-        <h1 className="text-xl sm:text-2xl text-gray-700 mb-4">
-          {amazonProduct.title}
-        </h1>
-        <p>{amazonProduct.description}</p>
-        <p>
-          <span className="font-bold">Rating: </span>
-        </p>
-        <p>
-          <span className="font-bold">Price: </span>
-          {amazonProduct.price}
-        </p>
-        <button className="button"> Add to Basket</button>
-      </div>
+        {/* Info Middle */}
+        <div className="col-span-2 flex flex-col space-y-3">
+          <div>
+            <h1 className="text-xl sm:text-2xl text-black">
+              {amazonProduct.title}
+            </h1>
+            <p className="text-gray-700">{amazonProduct.category}</p>
+          </div>
+          <p>{amazonProduct.description}</p>
+          <p>
+            <span className="font-bold">Rating: </span>
+          </p>
+          <p>
+            <span className="font-bold">Price: </span>
+            {amazonProduct.price}
+          </p>
+          <button className="button"> Add to Basket</button>
+        </div>
 
-      {/* Order option Right */}
-      <div>Order Options</div>
+        {/* Order option Right */}
+        {/* <div className="border-rounded border-2 col-span-2 ">
+          <h3>Order Options</h3>
+        </div> */}
+      </div>
     </div>
   );
 }
